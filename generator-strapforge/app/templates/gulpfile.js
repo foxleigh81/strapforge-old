efb01e7b48c7adb042741a6f95dd2ac380519ac1
Alexander Ward <alex.ward@bbqdigital.com>
2015-02-12 16:08:03 +0000

    updated gulpfile

--- generator-strapforge/app/templates/gulpfile.js
+++ generator-strapforge/app/templates/gulpfile.js
@@ -1,6 +1,13 @@
-var gulp = require('gulp');
-var browserSync = require('browser-sync');
-var reload = browserSync.reload;
+var gulp = require('gulp'),
+    browserSync = require('browser-sync');
+    compass    = require('gulp-compass'),
+    concat     = require('gulp-concat'),
+    imagemin   = require('gulp-imagemin'),
+    jshint     = require('gulp-jshint'),
+    notify     = require('gulp-notify'),
+    stripDebug = require('gulp-strip-debug'),
+    uglify     = require('gulp-uglify'),
+    reload = browserSync.reload;
 
 // watch files for changes and reload
 gulp.task('serve', function() {
@@ -10,11 +17,25 @@ gulp.task('serve', function() {
     }
   });
 
-  gulp.watch(['*.html', 'static/css/**/*.css', 'static/scripts/**/*.js'], { cwd: 'public' }, reload);
+  gulp.watch(['*.html', 'public/static/css/**/*.css', 'public/static/scripts/**/*.js'], { cwd: 'public' }, reload);
 });
 
+// Compress and minify images to reduce their file size
+gulp.task('images', function() {
+    var imgSrc = 'public/static/images/**/*',
+        imgDst = 'public/static/images';
+ 
+    return gulp.src(imgSrc)
+        .pipe(plumber({
+            errorHandler: onError
+        }))
+        .pipe(changed(imgDst))
+        .pipe(imagemin())
+        .pipe(gulp.dest(imgDst))
+        .pipe(notify({ message: 'Images task complete' }));
+
 gulp.task('jshint', function() {
-    return gulp.src('./src/scripts/*.js')
+    return gulp.src('src/js/*.js')
     .pipe(plumber({
         errorHandler: onError
     }))
