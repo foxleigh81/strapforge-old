6c8ff695385553efa5bb85e486ab049f746870bd
Alexander Ward <alex.ward@bbqdigital.com>
2015-02-12 20:49:29 +0000

    updated gulpfile

--- generator-strapforge/app/templates/gulpfile.js
+++ generator-strapforge/app/templates/gulpfile.js
@@ -8,7 +8,14 @@ var gulp = require('gulp'),
     notify     = require('gulp-notify'),
     stripDebug = require('gulp-strip-debug'),
     uglify     = require('gulp-uglify'),
-    reload     = browserSync.reload;
+    sass = require('gulp-ruby-sass'),
+    autoprefixer = require('gulp-autoprefixer'),
+    minifycss = require('gulp-minify-css'),
+    rename = require('gulp-rename'),
+    cache = require('gulp-cache'),
+    del = require('del');
+
+var reload     = browserSync.reload;
 
 // watch files for changes and reload
 gulp.task('serve', function() {
@@ -18,7 +25,40 @@ gulp.task('serve', function() {
     }
   });
 
+  // Compile SASS
+  gulp.watch('src/sass/**/*.scss', ['styles']);
+
+  // Compile JS
+  gulp.watch('src/js/**/*.js', ['scripts']);
+
+  // Optimise images
+  gulp.watch('public/static/images/*.*', ['images']);
+
   gulp.watch(['*.html', 'public/static/css/**/*.css', 'public/static/scripts/**/*.js'], { cwd: 'public' }, reload);
+});
+
+gulp.task('styles', function() {
+  return gulp.src('src/sass/*.scss')
+    .pipe(sass({ style: 'expanded' }))
+    .pipe(autoprefixer('last 2 version', 'safari 5', 'ie 8', 'ie 9', 'opera 12.1', 'ios 6', 'android 4'))
+    .pipe(concat('core.css'))
+    .pipe(gulp.dest('public/static/css'))
+    .pipe(rename({suffix: '.min'}))
+    .pipe(minifycss())
+    .pipe(gulp.dest('public/static/css'))
+    .pipe(notify({ message: 'Styles task complete' }));
+});
+
+gulp.task('scripts', function() {
+  return gulp.src('src/js/**/*.js')
+    .pipe(jshint('.jshintrc'))
+    .pipe(jshint.reporter('default'))
+    .pipe(concat('core.js'))
+    .pipe(gulp.dest('public/static/scripts'))
+    .pipe(rename({suffix: '.min'}))
+    .pipe(uglify())
+    .pipe(gulp.dest('public/static/scripts'))
+    .pipe(notify({ message: 'Scripts task complete' }));
 });
 
 // Compress and minify images to reduce their file size
@@ -34,17 +74,12 @@ gulp.task('images', function() {
         .pipe(imagemin())
         .pipe(gulp.dest(imgDst))
         .pipe(notify({ message: 'Images task complete' }));
-
-gulp.task('jshint', function() {
-    return gulp.src('src/js/*.js')
-    .pipe(plumber({
-        errorHandler: onError
-    }))
-    .pipe(jshint())
-    .pipe(jshint.reporter('default'))
-    .pipe(notify({ message: 'JS Hinting task complete' }));
 });
 
-gulp.task('custom', function() {
-  // place code for your default task here
+gulp.task('clean', function(cb) {
+    del(['public/static/css', 'public/static/scripts', 'public/static/images'], cb)
+});
+
+gulp.task('custom', ['clean'] function() {
+  gulp.start('styles', 'scripts', 'images')
 });
